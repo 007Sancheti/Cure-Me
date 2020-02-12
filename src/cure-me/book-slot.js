7a1457500f6e13fb0044969677464c088db042f5
Abhinav <001sancheti@gmail.com>
2020-02-12 20:10:33 +0530

    book slot completed

--- src/cure-me/book-slot.js
+++ src/cure-me/book-slot.js
@@ -4,6 +4,8 @@ import '@polymer/paper-card/paper-card.js';
 import '@polymer/paper-button/paper-button.js';
 import '@polymer/paper-dialog/paper-dialog.js';
 import '@polymer/paper-input/paper-input.js';
+import '@polymer/paper-toast/paper-toast.js';
+import '@polymer/app-route/app-location.js';
 /**
  * @customElement
  * @polymer
@@ -34,8 +36,12 @@ class BookSlot extends PolymerElement {
         {
           background:red;
         }
+        [hidden] {
+          display: none !important;
+        }      
       </style>
       <h2>You are Booking appointment for {{doctorName}}</h2>
+      <app-location route="{{route}}"></app-location>
       <ajax-call id="ajax"></ajax-call>
       <template is="dom-repeat" items={{slotDetails}}>
         <p class="slotDate">Slot Date: {{item.slotDate}}</p>
@@ -45,24 +51,31 @@ class BookSlot extends PolymerElement {
         <p>status {{slot.availableStatus}}</p>
         </div>
         <div class="card-actions"> 
-        <paper-button on-click="_handleBook" raised>Book Slot</paper-button> 
+        <paper-button on-click="_handleBook" data-set$={{item.slotDate}} raised>Book Slot</paper-button> 
         </div>
       </paper-card>
       </template>
       </template>
       <paper-dialog id="modal">
-      <iron-form id="form">
+      <iron-form id="form" hidden$={{hide}}>
       <form>
-          <paper-input type="number" id="mobileNo" label="Enter Mobile Number" required
+         <h3 id="slotSelected"></h3>
+          <paper-input id="mobileNo" type="text" maxlength="10" allowed-pattern=[0-9] label="Enter Mobile Number" required
               error-message="Mobile Number is required">Mobile Number</paper-input>
           <paper-input label="Enter Email ID" id="mail" required error-message="Email ID is required">Email ID</paper-input>
+          <paper-input label="Disease" id="disease"></paper-input>
       </form>
   </iron-form>
-  <div class="buttons">
+  <div class="buttons" hidden$={{hide}}>
   <paper-button id="cancel" dialog-dismiss>Cancel</paper-button>
     <paper-button on-click="_handleSubmit">OK</paper-button>
   </div>
+  <div hidden$={{!hide}}>
+<h2>{{message}}</h2>
+<paper-button on-click="_handleRoute" raised>Ok</paper-button>
+</div>
       </paper-dialog>
+      <paper-toast id="toast" text={{message}}></paper-toast>
     `;
   }
   static get properties() {
@@ -102,6 +115,26 @@ class BookSlot extends PolymerElement {
               }],
           }
         ]
+      },
+      slotTime:{
+        type:String,
+        value:''
+      },
+      slotDate:{
+        type:String,
+        value:''
+      },
+      slotId:{
+        type:Number,
+        value:0
+      },
+      doctorId:{
+        type:Number,
+        value:0
+      },
+      hide:{
+        type:Boolean,
+        value:false
       }
     };
   }
@@ -110,17 +143,52 @@ class BookSlot extends PolymerElement {
     let doctorDetails = JSON.parse(sessionStorage.getItem('selectedDoctor'));
     let { doctorName, doctorId } = doctorDetails;
     this.doctorName = doctorName;
-    this.$.ajax._makeAjaxCall('get', `${baseUrl}/cureme/doctors/${doctorId}/addSlot`, null, 'ajaxResponse')
+    this.doctorId=doctorId;
+    this.addEventListener('ajax-response',e=> this._slotsList(e))
+    this.addEventListener('book-slot',e=> this._confirmationMessage(e))
+    this.$.ajax._makeAjaxCall('get', `${baseUrl}/cureme/slots/doctors/${doctorId}/availableslots`, null, 'ajaxResponse')
+  }
+  _confirmationMessage(event)
+  {
+    this.hide=true;
+    console.log(event.detail.data.message)
+    this.message=event.detail.data.message;
+    // this.$.toast.open();
+  }
+  _handleRoute()
+  {
+    this.$.modal.close()
+    this.set('route.path','/patient-home')
+  }
+  _slotsList(event)
+  {
+    this.slotDetails=event.detail.data.availableDates;
   }
   _handleBook(event)
   {
+    let details=event.model.slot
+    this.slotTime=details.slotTime
+    this.$.slotSelected.innerHTML=this.slotTime
     this.$.modal.open();
-console.log(event)
+    this.slotDate=event.target.dataset.set
+    this.slotId=details.slotId
   }
   _handleSubmit()
   {
     let mobileNumber=this.$.mobileNo.value
     let emailId=this.$.mail.value;
+    let disease=this.$.disease.value;
+    let postObj={
+      disease,
+      "doctorId": this.doctorId,
+      emailId,
+      mobileNumber,
+      "slotDate":this.slotDate,
+      "slotId":this.slotId,
+      "slotTime":this.slotTime
+    }
+    console.log(postObj)
+    this.$.ajax._makeAjaxCall('post', `http://10.117.189.245:9090/cureme/users/bookslot`,postObj, 'bookSlot')
   }
 }
 
