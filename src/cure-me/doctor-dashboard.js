d521c78dea932cf8c9727999ee8d016558d14c5f
Shubh <guptas877@gmail.com>
2020-02-12 11:57:52 +0530

    commit

--- src/cure-me/doctor-dashboard.js
+++ src/cure-me/doctor-dashboard.js
@@ -1,5 +1,10 @@
 import {html, PolymerElement} from '@polymer/polymer/polymer-element.js';
-
+import '@polymer/paper-button/paper-button.js';
+import '@polymer/paper-input/paper-input.js';
+import '@polymer/paper-toast/paper-toast.js';
+import '@polymer/iron-form/iron-form.js';
+import './shared/API/ajax-call.js';
+import '@polymer/app-route/app-location.js';
 /**
  * @customElement
  * @polymer
@@ -11,7 +16,16 @@ class DoctorDashboard extends PolymerElement {
         :host {
           display: block;
         }
+        .form
+        {
+          background-image: linear-gradient(to top, #e6e9f0 0%, #eef1f5 100%);
+          width:40%;
+          margin:70px auto;
+          padding:15px;
+          box-shadow:0px 0px 5px 5px;
+        }
       </style>
+      <app-location route={{route}}></app-location>
       <form class="form">
      <h2>I am a</h2>
       <paper-button on-click="_handleClick" raised id="doctorLogin">Doctor</paper-button>
@@ -26,6 +40,9 @@ class DoctorDashboard extends PolymerElement {
         value: 'doctor-dashboard'
       }
     };
+  }
+  _handleClick(){
+
   }
 }
 
